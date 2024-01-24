e8f99d90c73bd6f7a05688cdf2f64c1e662ffdd5
ashifka <147649632+ashifka@users.noreply.github.com>
2024-01-24 14:18:12 +0000

    2

--- app.py
+++ app.py
@@ -2,28 +2,23 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-
-# Load the pre-trained model
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    features = [float(request.form['SL']),
-                float(request.form['SW']),
-                float(request.form['PL']),
-                float(request.form['PW'])]
-
-    # Make prediction using the loaded model
-    prediction = model.predict([features])[0]
-
-    return render_template('result.html', prediction=prediction)
+@app.route('/prediction', methods=['GET','POST'])
+def Predict():
+    if request.method == 'POST':
+        features = [float(request.form['SL']),
+                    float(request.form['SW']),
+                    float(request.form['PL']),
+                    float(request.form['PW'])]
+        model = pickle.load(open('model.pkl', 'rb'))
+        result = model.predict([features])[0]
+    return render_template('prediction.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
